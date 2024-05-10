c13fb59a77b1a647c39212bb1a5ea2dfae52f562
buoy <buoylee@gmail.com>
2024-05-10 22:05:33 +0800

    update Q11;

--- src/main/java/cn/buoy/leetcode/array/Q11.java
+++ src/main/java/cn/buoy/leetcode/array/Q11.java
@@ -2,21 +2,21 @@ package cn.buoy.leetcode.array;
 
 public class Q11 {
     /*
-    宽度只会越来越小, 如果高度不增加, 最大面积只可能越来越小.
+    思路: 宽度只会越来越小, 如果高度不增加, 最大面积只可能越来越小.
      */
     public int maxArea(int[] height) {
         if (height == null || height.length == 0) return 0;
-        int water = 0, l = 0, r = height.length - 1;
-        while (l < r) {
-            //保留需要移动的指针的当时高度.
-            int h = Math.min(height[l], height[r]);
-            water = Math.max(water, (r - l) * h);
+        int maxWaterVolumn = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            //取出2者中最矮的, 移动指针后用比它大的替换掉它.
+            int h = Math.min(height[left], height[right]);
+            maxWaterVolumn = Math.max(maxWaterVolumn, (right - left) * h);
             //移动需要移动的左或右指针, 直到找到比这个指针大的那个index.
-            while (l < r && height[l] <= h)
-                l++;
-            while (l < r && height[r] <= h)
-                r--;
+            while (left < right && height[left] <= h)
+                left++;
+            while (left < right && height[right] <= h)
+                right--;
         }
-        return water;
+        return maxWaterVolumn;
     }
 }
