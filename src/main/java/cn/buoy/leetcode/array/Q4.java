e7ebb52d0f88ff20453e6814cc18e6cdebc21b7b
buoy <buoylee@gmail.com>
2024-05-10 17:54:30 +0800

    update Q4;

--- src/main/java/cn/buoy/leetcode/array/Q4.java
+++ src/main/java/cn/buoy/leetcode/array/Q4.java
@@ -4,26 +4,28 @@ public class Q4 {
     /*
     https://www.youtube.com/watch?v=ScCg9v921ns
     O(log(min(1.length, 2.length)))
-    //fixme 边界 好烦
+    //fixme 边界 好烦(使得方便得到对应的中位数和num1, num2的左右用语比较的边界index)
+    //fixme 思路: 中位数(即位于整合后的有序数组的中间位置, 直接得出中间index,
+       再通过bin, 利用条件:num1Left <= num2Right && num2Left <= num1Right, 来找到答案位置.)
      */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
 
-        //选择较短的arr来2分,
+        //选择较短的arr作为num1来2分,
         if (m > n) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        //half total奇数时, 会 + 1, 中间;
+        //half total奇数时, 会 + 1, 中间; 为了方便取中位数, 就是当前中位数.
         //half total偶数时, 不变, 中间左边;
         int i = 0, j = 0, imin = 0, imax = m, half = (m + n + 1) / 2;
         double maxLeft = 0, minRight = 0;
         while (imin <= imax) {
             //奇数时, i为中, 0 + 5 / 2 = 2
             //偶数时, i为中间右边, 0 + 6 / 2 = 3
-            i = (imin + imax) / 2; //上边 左边个数
-            j = half - i; //下边 右边个数
+            i = (imin + imax) / 2; //num1 左边个数
+            j = half - i; //num2 左边个数
             if (j > 0 && i < m && nums2[j - 1] > nums1[i]) { //下边 左边最后一个 > 上边 右边第一个
                 imin = i + 1;
             } else if (i > 0 && j < n && nums1[i - 1] > nums2[j]) {
