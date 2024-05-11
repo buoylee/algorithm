7a493f7107dda2105c5ce8a9005a8801aeb7b1d8
buoy <buoylee@gmail.com>
2024-05-11 12:09:54 +0800

    update Q45;

--- src/main/java/cn/buoy/leetcode/array/Q45.java
+++ src/main/java/cn/buoy/leetcode/array/Q45.java
@@ -4,6 +4,7 @@ public class Q45 {
     /*
     https://www.youtube.com/watch?v=MknETKNOz_s
      */
+    //fixme 贪婪, 选步数范围内最大值, 当到达最范围内最远处累加步数.
     public int jump(int[] nums) {
         int len = nums.length;
         int step = 0, farestReachingIndex = 0, currentIndexFarestReachingIndex = 0;
