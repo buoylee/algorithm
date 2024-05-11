b89eae0e4b0264e93e6d6d187949dfc376207229
buoy <buoylee@gmail.com>
2024-05-11 14:00:20 +0800

    update Q53;

--- src/main/java/cn/buoy/leetcode/array/Q53.java
+++ src/main/java/cn/buoy/leetcode/array/Q53.java
@@ -4,17 +4,18 @@ public class Q53 {
     /**
      * https://www.youtube.com/watch?v=EcJ0wBBMoKA
      */
-    public int maxSubArray(int[] A) {
-        int n = A.length;
-        //到当前元素为止, 最大的subnum sum 值,
+    //fixme dp
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        //到当前元素为止, 最大的subarray sum 值,
         int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-        dp[0] = A[0];
+        dp[0] = nums[0];
         //max是 dp[i]的最大值.
         int max = dp[0];
 
         for (int i = 1; i < n; i++) {
             //最大值是 当前值 + 以往最大值(大于0的时候)
-            dp[i] = A[i] + (Math.max(dp[i - 1], 0));
+            dp[i] = nums[i] + (Math.max(dp[i - 1], 0));
             max = Math.max(max, dp[i]);
         }
 
