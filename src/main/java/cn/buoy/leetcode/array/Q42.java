2d683b2259f40d48a226a6caa7c6180e6de38d4d
buoy <buoylee@gmail.com>
2024-05-11 11:24:27 +0800

    update Q42;

--- src/main/java/cn/buoy/leetcode/array/Q42.java
+++ src/main/java/cn/buoy/leetcode/array/Q42.java
@@ -7,6 +7,10 @@ public class Q42 {
     当 l <= r, 可以算出l + 1 的 水高度, 因为右边再大, 也会受到l 的限制; 右边再小, 因为受 r 影响, 也不可能低于 l 高度.
     同理 l >= r.
      */
+    //fixme 思路: 储水需要一次完整的"高度递减+高度递增",
+    // 所以第一步需要找到递减趋势, 否则, 设置为maxleft作为再次起始点,
+    // 蓄水高度由低位决定.
+    // 中间无论高度如何, 都不会影响某个index的蓄水高度, 只由最左和最右高度影响, 注意看图就能知道.
     public int trap(int[] height) {
 //        int[] height= height;
         int len = height.length;
@@ -15,11 +19,12 @@ public class Q42 {
         int res = 0;
         int maxleft = 0, maxright = 0;
         while (left <= right) {
+            //如果左小,
             if (height[left] <= height[right]) {
-                //如果 [l] 比 maxleft都大时, [l] 无法储水. 并设置maxleft为当前高度.
+                //如果 [l] 比 maxleft都大时, [l] 无法储水起始点. 并设置maxleft为当前高度.
                 if (height[left] >= maxleft)
                     maxleft = height[left];
-                else
+                else //累加水量:
                     res += maxleft - height[left];
                 left++;
             } else {
@@ -60,6 +65,7 @@ public class Q42 {
             rightMax = Math.max(rightMax, height[right]);
 
             //how much can current position trap depends on the shorter bar (木桶原理)
+            //fixme 这里更加简洁, 因为当高度相同时, 累积量等于0, 省去上个例子判断需要等于.
             if (leftMax < rightMax) {
                 //DO NOT FORGET to subtract bar height of current position
                 result += leftMax - height[left];
