c5935a21a9263472db7becc818336aae7328e826
buoy <buoylee@gmail.com>
2024-06-14 15:19:48 +0800

    Q17.java Q31.java Q47.java

--- src/main/java/cn/buoy/leetcode/backtracking/Q17.java
+++ src/main/java/cn/buoy/leetcode/backtracking/Q17.java
@@ -9,24 +9,27 @@ public class Q17 {
      * BFS
      */
     public List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<String>();
-        if (digits.isEmpty()) return ans;
+        LinkedList<String> result = new LinkedList<String>();
+        if (digits.isEmpty()) return result;
         String[] mapping = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
+        //为了下边代码书写的一致(需要先弹出, len比较i)
+        result.add("");
         //注意看digit len, mapping 只是映射数字键而已!
-        //直到 传入的 digit len 到达最后, return
+        //直到 传入的 digits len(传入的数字的位数) 到达最后, return
         for (int i = 0; i < digits.length(); i++) {
-            //从头拆除digit
-            int x = Character.getNumericValue(digits.charAt(i));
-            //从 ans 元素len == 0 开始, 只要 ans 元素len == digits index, 就会append digit.
-            while (ans.peek().length() == i) {
-                String t = ans.remove();
-                for (char s : mapping[x].toCharArray())
-                    //append完 推入,
-                    ans.add(t + s);
+            //从头拆除digits
+            int digit = Character.getNumericValue(digits.charAt(i));
+            //从 result 元素len == 0("") 开始,
+            //len0: ""->remove ""->a, b, c;
+            //len1: a, b, c->remove a, b, c-> a + d, a + e, a + f,...
+            while (result.peek().length() == i) {
+                String preText = result.remove();
+                for (char letter : mapping[digit].toCharArray())
+                    //例: a + d; a + e; a + f;
+                    result.add(preText + letter);
             }
         }
-        return ans;
+        return result;
     }
 
 
@@ -41,9 +44,9 @@ public class Q17 {
 
     /**
      * @param digits 入口传入的数字
-     * @param list   结果
+     * @param list   结果(一个完整的sb从digit[0]~digit[len-1]的append完后, 放入list)
      * @param map    映射的字母arr
-     * @param sb     temp str
+     * @param sb     temp str(一个组合的temp str, 例: adg)
      * @param start  当前添加 第 start 位(0~digits.len) 数字
      */
     private void backtrack(String digits, List<String> list, char[][] map, StringBuilder sb, int start) {
@@ -55,10 +58,13 @@ public class Q17 {
         //取传入数字 第i位 digit,
         int num = digits.charAt(start) - '0';
         for (int i = 0; i < map[num].length; i++) {
+            //例: a
             sb.append(map[num][i]);
             //刚 拼接好的str 和 下一位要加入的index 传入.
+            // 例: ad
             backtrack(digits, list, map, sb, start + 1);
             //backtracking 一下
+            //例" a
             sb.deleteCharAt(sb.length() - 1);
         }
     }
--- src/main/java/cn/buoy/leetcode/backtracking/Q47.java
+++ src/main/java/cn/buoy/leetcode/backtracking/Q47.java
@@ -13,6 +13,7 @@ public class Q47 {
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
+        //需要先排序, 为了处理相同数字重复组合问题. 解释在下边.
         Arrays.sort(nums);
         backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
         return list;
@@ -23,7 +24,10 @@ public class Q47 {
             list.add(new ArrayList<>(tempList));
         } else {
             for (int i = 0; i < nums.length; i++) {
-                //在挑选某位digit的时候, 如果(当前元素与前一个元素相同), 如果(前一个元素使用 后一个不使用), 会与 (前一个元素没使用,而后一个元素使用了)重复, 所以要排除一种, 这里选择排除后者.
+                //~~在挑选某位digit的时候, 如果(当前元素与前一个元素相同), 如果(前一个元素使用 后一个不使用), 会与 (前一个元素没使用,而后一个元素使用了)重复, 所以要排除一种, 这里选择排除后者.~~
+                //关键:
+                //used[i]用来限制在同一层(相同位置)不能出现再出现相同数字
+                //i > 0 && nums[i] == nums[i - 1] && !used[i - 1], 因为nums排过序, 这样使得相同的数字使用时, 必须从左到右一个个使用, 使得相同数字排列顺序唯一(不会先使用后边再用前边, 造成相同的nums组合出现).
                 if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
                 used[i] = true;
                 tempList.add(nums[i]);
