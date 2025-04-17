465d669e67fc2835f23be780588c423b2f8c81eb
buoylee <buoylee@gmail.com>
2025-04-17 23:59:45 +0800

    update;

--- src/main/java/cn/buoy/leetcode/string/Q3.java
+++ src/main/java/cn/buoy/leetcode/string/Q3.java
@@ -4,29 +4,26 @@ import java.util.HashMap;
 
 public class Q3 {
     /**
-     * 簡單, 關鍵點, 要想清楚.
+     * 簡單
      * https://www.youtube.com/watch?v=fBiiKy8kwaY
      * 思路: 當出現有相同字母的時候, 要去掉前邊所有的substr.
      */
     public int lengthOfLongestSubstring(String s) {
         // map<字母 , 這個字母的index位置>
         HashMap<Character, Integer> map = new HashMap<>();
-        int maxSubstrLen = 0;
+        int result = 0;
         int left = 0;
         for (int i = 0; i < s.length(); i++) {
             char letter = s.charAt(i);
             // 遇到同字母,
             if (map.containsKey(letter))
-                // 取相同字母的上一個index +1, 但是要取至今最大那個.
-                // 關鍵: 思考上, 很簡單, 就是 上一個相同字母的下一個,
-//                if (map.get(letter) + 1 > left)
-//                    left = map.get(letter) + 1;
-                // 這個寫法有點反應不過來.
+                // 先取上一個相同字母的index, +1;
+                // 但是, 之前可能有其他重复的字母, 已经截取更右的index了, 所以需要再次比较left.
                 left = Math.max(left, map.get(letter) + 1);
-            maxSubstrLen = Math.max(maxSubstrLen, i - left + 1);
+            result = Math.max(result, i - left + 1);
             map.put(letter, i);
         }
-        return maxSubstrLen;
+        return result;
     }
 
     public static void main(String[] args) {
